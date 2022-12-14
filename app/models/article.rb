02e1a38406c4f600cd754b43ee82212a190ed00a
miroslav <miroslav@kolosek.com>
2022-12-14 10:31:58 +0100

    Add statuses

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,18 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
+
+  def self.public_count
+    where(status: 'public').count
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
+
+  def public_count
+    where(status: 'public').count
+  end
 end
