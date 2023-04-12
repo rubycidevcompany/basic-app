74cb0382e66ae6aa553de538951dabbc9f73f276
SasaV-Kolosek <sasavas@kolosek.com>
2023-04-12 10:56:47 +0200

    with rubocop fail

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "Item" do
+RSpec.describe 'Item' do
   let(:item) {
     Item.create!(
-      name: "MyString"
+      name: 'MyString'
     )
   }
 
-  it "test something" do
+  it 'test something' do
     expect(item).to be_valid
   end
 
-  it "test something 2" do
+  it 'test something 2' do
     expect(true).to be true
   end
 
-  it "test something 3" do
+  it 'test something 3' do
     expect(true).to be true
   end
 end
