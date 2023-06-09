fb95a51c7b2c32d24e5e06e2bba48418b8f9b49c
SasaV-Kolosek <sasavas@kolosek.com>
2023-06-09 17:49:36 +0200

    test selenium with failed test

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -11,5 +11,10 @@ RSpec.describe 'Test Selenium driver', type: :feature, js: true do
       visit new_item_path
       expect(page).to have_current_path(new_item_path)
     end
+
+    it "test failed test" do
+      visit new_item_path
+      expect(page).to have_current_path(items_path)
+    end
   end
 end
