8dd080caeace7e17ed0d20ffb06151b83b14035c
SasaV-Kolosek <sasavas@kolosek.com>
2023-09-18 15:20:21 +0200

    near 100 tests

---
+++ spec/test_spec_2.rb
@@ -0,0 +1,732 @@
+require 'rails_helper'
+
+RSpec.describe 'Item' do
+  let(:item) do
+    Item.create!(
+      name: 'MyString'
+    )
+  end
+
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+  it 'test something' do
+    expect(item).to be_valid
+  end
+
+  it 'test something 2' do
+    expect(true).to be true
+  end
+
+  it 'test something 3' do
+    expect(true).to be true
+  end
+
+  it 'test something 4' do
+    skip("test skipped")
+    expect(true).to be true
+  end
+
+  it 'test something 5' do
+    expect(true).to be true
+  end
+
+  it 'test something 6' do
+    expect(true).to be true
+  end
+end
+
+   
